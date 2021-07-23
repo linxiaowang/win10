1fbc09847e683b9ced77c5750e4ec25e74ab0523
linxiaowang <1104220717@qq.com>
2021-07-23 13:49:43 +0800

    项目结构上传

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+
+const path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', resolve('./src'))
+      .set('assets', resolve('./src/assets'))
+      .set('store', resolve('./src/store'))
+      .set('router', resolve('./src/router'))
+      .set('plugins', resolve('./src/plugins'))
+      .set('comp', resolve('./src/components'))
+      .set('styles', resolve('./src/styles'))
+  }
+}
